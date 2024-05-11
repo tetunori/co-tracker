0cae3d3c20356698ceca198affa134cec9bc4224
tetunori <tetunori@gmail.com>
2024-05-11 18:30:48 +0900

    Update visualizer.py

--- cotracker/utils/visualizer.py
+++ cotracker/utils/visualizer.py
@@ -18,6 +18,7 @@ import csv
 header = ['frame', 'ptIdx', 'x', 'y']
 with open('test.csv', 'w', newline='') as f:
   f.truncate(0)
+  writer = csv.writer(f)
   writer.writerow(header)
 
 def read_video_from_path(path):
@@ -277,6 +278,7 @@ class Visualizer:
                         # print("frame: %d, index:%d, x:%d, y:%d" % (t, i, coord[0], coord[1]))
                         data = [t, i, coord[0], coord[1]]
                         with open('test.csv', 'w', newline='') as f:
+                          writer = csv.writer(f)
                           writer.writerow(data)
             res_video[t] = np.array(img)
 
